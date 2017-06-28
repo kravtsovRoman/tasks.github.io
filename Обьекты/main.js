fe44bef443723e050a6c55289b538927737987ec
Kravtsov Roman <rozario91208@gmail.com>
2017-06-28 19:42:51 +0300

    only up, to the knowledge

--- Обьекты/main.js
+++ Обьекты/main.js
@@ -1,23 +1,48 @@
 // 1. Calculator
 
-var calculator = {
+// var calculator = {
 
-	 firstNum : 0,
-	secondNum : 0,
+// 	 firstNum : 0,
+// 	secondNum : 0,
 
-	read : function(){
-		this.firstNum = prompt('Imput first number');
-		this.secondNum = prompt('Imput second number');
+// 	read : function(){
+// 		this.firstNum = prompt('Imput first number');
+// 		this.secondNum = prompt('Imput second number');
+// 	},
+
+// 	suma : function(){
+// 		return new Number(this.firstNum) + Number(this.secondNum);
+// 	},
+// 	mul : function(){
+// 		return new Number(this.firstNum) * Number(this.secondNum);
+// 	}
+// }
+
+// calculator.read();
+// console.log( calculator.suma() );
+// console.log( calculator.mul() );
+
+
+// 2. ladder, chain method
+
+var ladder = {
+	step: 0,
+
+	up: function(){
+		this.step++;
+		return this;
 	},
+	down: function(){
+		this.step--;
+		return this;
 
-	suma : function(){
-		return new Number(this.firstNum) + Number(this.secondNum);
 	},
-	mul : function(){
-		return new Number(this.firstNum) * Number(this.secondNum);
+	showStep: function(){
+		console.log(this.step);
+		return this;
 	}
-}
+};
 
-calculator.read();
-console.log( calculator.suma() );
-console.log( calculator.mul() );
+ladder.up().up().up().up().down().down().showStep();
+ladder.up().up().up().up().down().down().showStep();
+ladder.up().up().up().up().down().down().showStep();
