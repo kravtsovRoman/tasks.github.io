127c22a06a531e62dd3f101b6cf73e5d88cddf67
Kravtsov Roman <rozario91208@gmail.com>
2017-06-28 19:33:14 +0300

    first step with Objects

--- Обьекты/main.js
+++ Обьекты/main.js
@@ -1,13 +1,23 @@
 // 1. Calculator
 
 var calculator = {
-	firstNum : 0,
+
+	 firstNum : 0,
 	secondNum : 0,
+
 	read : function(){
 		this.firstNum = prompt('Imput first number');
 		this.secondNum = prompt('Imput second number');
+	},
+
+	suma : function(){
+		return new Number(this.firstNum) + Number(this.secondNum);
+	},
+	mul : function(){
+		return new Number(this.firstNum) * Number(this.secondNum);
 	}
 }
 
 calculator.read();
-console.log(calculator.firstNum);
+console.log( calculator.suma() );
+console.log( calculator.mul() );
