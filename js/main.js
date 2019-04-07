980401c5a94901125673e902e593cc1dbba3dbba
Roman <rozario91208@gmail.com>
2019-04-07 16:31:23 +0300

    new version : slice long string

--- js/main.js
+++ js/main.js
@@ -4,13 +4,10 @@ var str = 'Very dfghjkldas ashdg jadgs hjdk asd';
 var truncate = function (str, maxlength) {
     var newStr = '';
 
-    if (str.length > maxlength) {
-        newStr = str.slice(0, maxlength - 3) + '...';
-    }
-    else {
-        newStr = str;
-    }
-    console.log(newStr);
+    return str.length > maxlength ?
+        str.slice(0, maxlength - 3) + '...' : str
 }
 
-truncate(str, 10)
+console.log(
+    truncate(str, 10)
+);
