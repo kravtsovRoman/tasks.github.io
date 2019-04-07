1ebec9966d9f823ff4c1d20e7c2272a2bdf4b749
Roman <rozario91208@gmail.com>
2019-04-07 13:39:15 +0300

    indexOF

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
 
-let n = function (min, max) {
-    return min + Math.random() * (max - min)
-}
+var str = "Ослик Иа-Иа посмотрел на виадук  Иа-Иа"; // ищем в этой строке
+var target = "Иа"; // цель поиска
 
+var pos = -1;
+while ((pos = str.indexOf(target, pos + 1)) != -1) {
+    console.log(pos);
+}
 console.log(
 
-    +n(50, 100).toFixed(0)
+
+
 
 );
