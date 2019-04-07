7c44886ac454ea2ce1f95ead58e3d051b907e490
Roman <rozario91208@gmail.com>
2019-04-07 14:06:48 +0300

    slice()

--- js/main.js
+++ js/main.js
@@ -1,14 +1,9 @@
 
-var str = "Ослик Иа-Иа посмотрел на виадук  Иа-Иа"; // ищем в этой строке
-var target = "Иа"; // цель поиска
+var str = "1234567891011121314151617181920";
 
-var pos = -1;
-while ((pos = str.indexOf(target, pos + 1)) != -1) {
-    console.log(pos);
-}
 console.log(
 
 
-
+    str.slice(13, -3)
 
 );
