03a9aa178a9e7cda1b6507347e368b6d4b04018d
Roman <rozario91208@gmail.com>
2019-04-29 13:45:53 +0300

    array add class

--- js/main.js
+++ js/main.js
@@ -1,12 +1,23 @@
 
-var user = {};
+var obj = {
+    className: 'open menu'
+};
 
-// user.name = "Vasya";
-
-var isEmpty = function (obj) {
-    Object.keys(obj).length ? console.log('Not empty') : console.log('Empty');
+function addClass(obj, cls) {
+    var arrClass = obj.className.split(' ');
+    for (var i = 0; i < arrClass.length; i++) {
+        if (arrClass[i] === cls) {
+            break;
+        }
+        else {
+            arrClass.push(cls);
+            break;
+        }
+    }
+    obj.className = arrClass.join(' ');
+    return obj;
 }
 
-isEmpty(user);
-
-
+console.log(addClass(obj, 'new'))
+console.log(addClass(obj, 'me'))
+console.log(addClass(obj, 'abc'))
