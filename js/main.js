22628e84543f3a8d62f8a5f3383d14e4b6a15604
Roman <rozario91208@gmail.com>
2019-04-29 16:34:39 +0300

    sortReverse

--- js/main.js
+++ js/main.js
@@ -1,16 +1,14 @@
 arr = [5, 3, 8, 1, 10, 15, 22, 123, 0, 3, 12, 14, 2, 3, 4, 5555, 12, 2];
 
-function filterRangeInPlace(arr, a, b) {
-    // console.log(b)
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] < a || arr[i] > b) {
-            arr.splice(i, 1);
-            i--;
-            // console.log(i)
-        }
+function sortReverse(arr) {
+    arr.sort(compareNumeric).reverse()
 
-    }
     return arr;
 }
 
-console.log(filterRangeInPlace(arr, 10, 20))
+function compareNumeric(a, b) {
+    if (a > b) return 1;
+    if (a < b) return -1;
+}
+
+console.log(sortReverse(arr))
