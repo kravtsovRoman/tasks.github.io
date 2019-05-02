b93765c55ff58034ce5d0d390de147800c74ed6e
Roman <rozario91208@gmail.com>
2019-05-02 22:17:24 +0300

    new Date()

--- js/main.js
+++ js/main.js
@@ -1,11 +1,27 @@
+var arr = [];
+for (var i = 0; i < 1000; i++) arr[i] = 0;
 
-
-function f() {
-    var summ = 0;
-    for (var i = 0; i < arguments.length; i++) {
-        summ += arguments[i];
-    }
-    return summ;
+function walkIn(arr) {
+    for (var key in arr) arr[key]++;
 }
 
-console.log(f(1, 2, 3, 4, 5, 5));
+function walkLength(arr) {
+    for (var i = 0; i < arr.length; i++) arr[i]++;
+}
+
+function bench(f) {
+    for (var i = 0; i < 10000; i++) f(arr);
+}
+
+console.time("All Benchmarks");
+
+console.time("walkIn");
+bench(walkIn);
+console.timeEnd("walkIn");
+
+console.time("walkLength");
+bench(walkLength);
+console.timeEnd("walkLength");
+
+console.timeEnd("All Benchmarks");
+
