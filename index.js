ecef2cbb981045df476e12fdf214c9371c75466c
Roman <rozario91208@gmail.com>
2020-04-20 22:33:41 +0300

    connect Handlebars for views

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 const express = require('express');
 const path = require('path');
+const expressHandlebars = require('express-handlebars');
+
 const app = express();
+
+const hbs = expressHandlebars.create({
+  defaultLayout: 'main',
+  extname: 'hbs'
+});
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
 
 
 app.get('/', (req, res) => {
-  res.status(200);
-  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+  res.render('index');
 
-})
+});
+
+app.get('/about', (req, res) => {
+  res.render('about')
+
+});
 
 
 
