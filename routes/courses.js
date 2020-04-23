1e10d7a2ba8da1a6e246a5985f4e0fd4303caa4e
Roman <rozario91208@gmail.com>
2020-04-23 22:44:26 +0300

    add and output courses

--- routes/courses.js
+++ routes/courses.js
@@ -1,11 +1,16 @@
 const { Router } = require('express');
+const Course = require('../models/course');
 const router = Router();
 
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+
+  const courses = await Course.getAll();
+
   res.render('courses', {
     title: "Курсы",
-    isCoursesPage: true
+    isCoursesPage: true,
+    courses
   })
 });
 
